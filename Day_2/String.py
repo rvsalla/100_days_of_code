6492a82a763e0902f6d1bfe86ff816ac6ed877aa
Romulo Salla <rvsalla@gmail.com>
2024-12-01 19:49:46 -0300

    Add new modules

---
+++ Day_2/String.py
@@ -0,0 +1,11 @@
+print("Hello"[0])
+print("Hello"[-1])
+
+print(type(122))
+
+print(2**2)
+
+a = 1.9
+b = "alerto"
+
+print(f"{b} tem {a} de altura")
---
+++ Day_3/Logic.py
@@ -0,0 +1,6 @@
+height = int(input("what is your height in cm?: "))
+
+if height >=120:
+    print("you can pass")
+else:
+    print("you sall not pass")
