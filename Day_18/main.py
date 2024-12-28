4b3b47b34fd86cfb05dbce4aee870c82967c84bb
Romulo Salla <rvsalla@gmail.com>
2024-12-28 20:18:11 -0300

    Add day 18

---
+++ Day_18/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("circle")
+timmy.color('red')
+for i in range(4):
+    timmy.forward(100)
+    timmy.right(90)
+#timmy.forward(100)
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
