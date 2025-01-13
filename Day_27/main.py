b50071d377a2208070b055674f1ea09eafffb512
Romulo Salla <rvsalla@gmail.com>
2025-01-13 20:53:38 -0300

    Add tkinter project (part 1)

---
+++ Day_27/main.py
@@ -0,0 +1,16 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("GUI Program")
+window. minsize(width= 600, height= 400)
+
+#lable
+my_lable = tkinter.Label(text="First Lable", font=("Arial", 24, "bold"))
+my_lable.pack()
+
+
+
+
+
+
+window.mainloop()
--- Test.py
+++ Test.py
@@ -1,1 +1,8 @@
-print("Hi")
+def add(*args): 
+    sum = 0
+    for n in args:
+        sum += n
+    print(sum)
+ 
+my_list = [1,2,3,4,5,6,7,8,9]
+add(1,2,3,4,5,6,7,8,9)
