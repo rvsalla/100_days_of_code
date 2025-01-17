56a0553dcd7d7113029105a1bf3c40e35bcb460b
Romulo Salla <rvsalla@gmail.com>
2025-01-16 22:13:39 -0300

    Add tkinter miles to Km project

--- Day_27/main.py
+++ Day_27/main.py
@@ -1,16 +1,30 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.title("GUI Program")
-window. minsize(width= 600, height= 400)
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles*1.609)
+    km_result_lable.config(text=f"{km}")
 
-#lable
-my_lable = tkinter.Label(text="First Lable", font=("Arial", 24, "bold"))
-my_lable.pack()
+window = Tk()
+window.title("Mile to Km Converter")
+window.config(padx=20,pady=20)
 
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
 
+miles_lable = Label(text="Miles")
+miles_lable.grid(column=2, row=0)
 
+equals_lable = Label(text="is equals to")
+equals_lable.grid(column=0, row=1)
 
+km_result_lable = Label(text="0")
+km_result_lable.grid(column=1, row=1)
 
+km_lable = Label(text="Km")
+km_lable.grid(column=2, row=1)
+
+calculate_button = Button(text="Calculate", command=miles_to_km) 
+calculate_button.grid(column=1, row=2)
 
 window.mainloop()
--- Test.py
+++ Test.py
@@ -2,7 +2,6 @@ def add(*args):
     sum = 0
     for n in args:
         sum += n
-    print(sum)
+    print(type(args))
  
-my_list = [1,2,3,4,5,6,7,8,9]
 add(1,2,3,4,5,6,7,8,9)
